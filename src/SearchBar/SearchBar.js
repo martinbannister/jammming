88e04f21bdf0b9e0eacb8fcb9682a4771fe9ed47
martinbannister <martin.bannister91@gmail.com>
2024-06-13 17:01:33 +0100

    FEAT(SearchBar): begin SearchBar component

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+
+    return (
+        <>
+            <input type='text' />
+        </>
+    );
+}
