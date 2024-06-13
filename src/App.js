2fb6779303520571d11f472ecc6bb88c66659cc8
martinbannister <martin.bannister91@gmail.com>
2024-06-13 20:19:21 +0100

    FEAT(App): add SearchBar to App return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
