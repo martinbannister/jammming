878b14c50110d1e9bc37949f9c6f5a75f799ae8c
martinbannister <martin.bannister91@gmail.com>
2024-06-13 21:33:18 +0100

    FEAT(App): add SearchResults component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar/SearchBar';
+import SearchResults from './SearchResults/SearchResults';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      
+      <SearchResults />
     </div>
   );
 }
