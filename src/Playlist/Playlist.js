5562d57e691863af279ad0baead9056de5fab29c
martinbannister <martin.bannister91@gmail.com>
2024-06-14 20:19:45 +0100

    FEAT(Playlist): inital commit for component and styles

---
+++ src/Playlist/Playlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Playlist.module.css';
+
+function Playlist(props) {
+    
+    return (
+        <>
+            <section className="container">
+                <p>Something</p>
+            </section>
+        </>
+    )
+}
+
+export default Playlist;
