1f0af091c091aeabc4fce372e1dc2623c2f65543
martinbannister <martin.bannister91@gmail.com>
2024-06-14 20:40:37 +0100

    FEAT(Tracklist): add default export & correct class name

--- src/Tracklist/Tracklist.js
+++ src/Tracklist/Tracklist.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Tracklist.module.css';
+
+function Tracklist(props) {
+    return (
+        <>
+            <section className="container">
+                
+            </section>
+        </>
+    );
+}
+
+export default Tracklist;
