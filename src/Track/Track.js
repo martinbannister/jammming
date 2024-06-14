169f5feb8f49dc64e042f8be5e324fd55b04e0a1
martinbannister <martin.bannister91@gmail.com>
2024-06-14 20:21:31 +0100

    FEAT(Track): inital commit for component and styles

---
+++ src/Track/Track.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Track.module.css';
+
+function Track(props) {
+    return (
+        <>
+            <div className='track_card'>
+                
+            </div>
+        </>
+    );
+}
+
+export default Track;
