db03bc04ada672c80378ec65af0181438fac7543
martinbannister <martin.bannister91@gmail.com>
2024-06-14 21:33:55 +0100

    FEAT(Track): render data passed to track components

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Track.module.css';
 
-function Track(props) {
+function Track({data}) {
     return (
         <>
             <div className='track_card'>
-                
+                <ul key={data.id}>
+                  <li>{data.name}</li>
+                  <li>{data.artist}</li>
+                  <li>{data.album}</li>
+                  <li>{data.id}</li>
+                </ul>
             </div>
         </>
     );
