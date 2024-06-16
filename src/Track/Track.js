cd9ba4bb9bd11f6986a88ffc63211ed095da68e6
martinbannister <martin.bannister91@gmail.com>
2024-06-16 21:18:20 +0100

    FIX(Track): try to make list item keys unique

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -6,10 +6,10 @@ function Track({data}) {
         <>
             <div className='track_card'>
                 <ul key={data.id}>
-                  <li>{data.name}</li>
-                  <li>{data.artist}</li>
-                  <li>{data.album}</li>
-                  <li>{data.id}</li>
+                  <li key={data.id+'1'}>{data.name}</li>
+                  <li key={data.id+'2'}>{data.artist}</li>
+                  <li key={data.id+'3'}>{data.album}</li>
+                  <li key={data.id+'4'}>{data.id}</li>
                 </ul>
             </div>
         </>
