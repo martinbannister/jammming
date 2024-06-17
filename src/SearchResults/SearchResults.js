fba5b937b341f38264630f189b6923a535dc265b
martinbannister <martin.bannister91@gmail.com>
2024-06-17 15:03:11 +0100

    CSS(SearchResutls): move css styling to styling objects in the container

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './SearchResults.module.css';
 import Track from '../Track/Track';
 
+const styleContainer = {
+  position: 'absolute',
+  top: '100px',
+  width: '30dvw',
+  height: 'calc(90dvh - 110px',
+  margin: '50px',
+  border: '1px solid green',
+  boxShadow: 'rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px',
+  overflow: 'scroll'
+}
+
 function SearchResults({searchResults}) {
     const tracks = searchResults.map( track => {
       return <Track data={track} />
@@ -9,7 +20,7 @@ function SearchResults({searchResults}) {
   
     return (
     <>
-        <section className='container'>
+        <section style={styleContainer}>
           {tracks}
         </section>
     </>);
