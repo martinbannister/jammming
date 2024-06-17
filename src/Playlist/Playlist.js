e90b6ba34f1ceebdf2d948820d5232272013a334
martinbannister <martin.bannister91@gmail.com>
2024-06-17 20:55:48 +0100

    FEAT(Playlist): import Tracklist, add to return and pass in track data

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './Playlist.module.css';
+import Tracklist from '../Tracklist/Tracklist';
+
+const styleContainer = {
+  position: 'absolute',
+  top: '100px',
+  width: '30dvw',
+  height: 'calc(90dvh - 110px',
+  margin: '50px',
+  border: '1px solid green',
+  boxShadow: 'rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px, rgba(240, 46, 170, 0.05) 25px 25px',
+  overflow: 'scroll'
+}
 
 function Playlist(props) {
     
     return (
       <>
-        <section className="header">
-          
-        </section>
-        <section className="container">
-          <p>Something</p>
-        </section>
+        <div style={styleContainer}>
+          <section className="header">
+            <p>HEADER</p>
+          </section>
+          <section className="container">
+            <Tracklist trackData={props.data} />
+          </section>
+        </div>
       </>
     )
 }
