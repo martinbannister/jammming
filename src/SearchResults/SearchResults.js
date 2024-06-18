a1502bbf8183a47840d87fbdaa2334ba809b916e
martinbannister <martin.bannister91@gmail.com>
2024-06-18 15:22:01 +0100

    CSS(SearchResults): add textAlign to styleContainer

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -10,7 +10,8 @@ const styleContainer = {
   margin: '50px',
   border: '1px solid green',
   boxShadow: 'rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px',
-  overflow: 'scroll'
+  overflow: 'scroll',
+  textAlign: 'left'
 }
 
 function SearchResults({searchResults}) {
